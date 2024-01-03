c60842a8953879c28fdb67b3e4c2202eac23ef40
Cristopher Areche <cristopherareche764@gmail.com>
2024-01-02 23:00:47 -0400

    landing page section 1 completed

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -10,6 +10,9 @@ import hero3 from "../assets/hero_2.jpg";
 import circle from "../assets/Ellipse.svg";
 import { RiCloseFill, RiMenu3Fill } from "react-icons/ri";
 import LandingPageSideBar from "../components/LandingPageSideBar";
+import { LuCalendarCheck } from "react-icons/lu";
+import { BsHospital } from "react-icons/bs";
+import { PiTooth } from "react-icons/pi";
 
 const Landing = () => {
   return (
@@ -57,7 +60,7 @@ const Landing = () => {
         </div>
       </div>
       {/* Hero */}
-      <div className="flex justify-center h-screen w-screen  pt-20 px-[8em] bg-gradient-to-r from-[#0E264B] to-[#3e66a1] ">
+      <div className="flex justify-center h-[100vh] min-w-[100vw] pt-24 pb-8 px-[8em] bg-gradient-to-r from-[#0E264B] to-[#3e66a1] ">
         <div className="flex justify-start gap-10  ">
           {/* left */}
           <div className="hidden md:flex justify-center gap-14 w-[70%]">
@@ -132,7 +135,70 @@ const Landing = () => {
           </div>
         </div>
       </div>
-      <LandingPageSideBar />
+
+      {/* Section 1 */}
+      <div className="flex flex-col md:flex-row justify-center max-w-screen md:h-screen  pt-20 px-10 text-center md:text-start md:px-[8em] bg-background-300">
+        {/* left side */}
+        <div className="flex flex-col gap-8 w-full md:w-1/2">
+          <h1 className="text-[3em] md:text-[5em] font-bold text-background-100 leading-[1em]">
+            Get pearl white teeth
+          </h1>
+          <img
+            className="rounded-t-full hidden md:flex"
+            style={{ height: "40em", width: "30em" }}
+            src={hero3}
+            alt="hero"
+          />
+        </div>
+        {/* right side */}
+        <div className="flex flex-col gap-8 w-full md:w-1/2">
+          <p className="text-xl text-gray-500 py-10">
+            We have service by contacting the doctor online for consultation
+            about your dental health or asking for work schedules at the clinic
+          </p>
+
+          <div className="flex flex-col gap-3 justify-start">
+            <div className="text-[4em] text-icon-100 flex">
+              <LuCalendarCheck />
+            </div>
+            <h3 className="text-2xl font-bold text-background-100 text-start">
+              Book Online
+            </h3>
+            <p className="text-gray-500 text-xl text-start">
+              Book online - quickly see when we&apos;re available and book a
+              time for the whole family in under 5 minutes. It's so easy.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-3 justify-start">
+            <div className="text-[4em] text-icon-100 flex justify-end md:justify-start">
+              <BsHospital />
+            </div>
+            <h3 className="text-2xl font-bold text-background-100 text-end md:text-start">
+              Visit Clinic
+            </h3>
+            <p className="text-gray-500 text-xl text-end md:text-start">
+              Visit our clinic - the kids even have their own private play area
+              dedicated children&apos;s room.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-3 justify-start">
+            <div className="text-[4em] text-icon-100">
+              <PiTooth />
+            </div>
+            <h3 className="text-2xl font-bold text-background-100 text-start">
+              Get Treatment
+            </h3>
+            <p className="text-gray-500 text-xl text-start">
+              Get to treat your teeth with a satisfying experience and
+              professional service from an expert doctor.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* <LandingPageSideBar /> */}
     </div>
   );
 };
